2191e22d681c980d9701e83ae9a7edaca29c4100
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-11 00:18:30 +0530

    Defined model class for vehicle manufacturers

---
+++ lib/models/vehicle_manufacturer.dart
@@ -0,0 +1,29 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'vehicle_manufacturer.g.dart';
+
+@JsonSerializable()
+class VehicleManufacturer {
+  @JsonKey(name: 'Country')
+  String country;
+
+  @JsonKey(name: 'Mfr_CommonName')
+  String manufacturerCommonName;
+
+  @JsonKey(name: 'Mfr_ID')
+  int manufacturerID;
+
+  @JsonKey(name: 'Mfr_Name')
+  String manufacturerName;
+
+  VehicleManufacturer(
+      {required this.country,
+      required this.manufacturerCommonName,
+      required this.manufacturerID,
+      required this.manufacturerName});
+
+  factory VehicleManufacturer.fromJson(Map<String, dynamic> json) =>
+      _$VehicleManufacturerFromJson(json);
+
+  Map<String, dynamic> toJson() => _$VehicleManufacturerToJson(this);
+}
---
+++ lib/models/vehicle_manufacturer.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'vehicle_manufacturer.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+VehicleManufacturer _$VehicleManufacturerFromJson(Map<String, dynamic> json) =>
+    VehicleManufacturer(
+      country: json['Country'] as String,
+      manufacturerCommonName: json['Mfr_CommonName'] as String,
+      manufacturerID: json['Mfr_ID'] as int,
+      manufacturerName: json['Mfr_Name'] as String,
+    );
+
+Map<String, dynamic> _$VehicleManufacturerToJson(
+        VehicleManufacturer instance) =>
+    <String, dynamic>{
+      'Country': instance.country,
+      'Mfr_CommonName': instance.manufacturerCommonName,
+      'Mfr_ID': instance.manufacturerID,
+      'Mfr_Name': instance.manufacturerName,
+    };
