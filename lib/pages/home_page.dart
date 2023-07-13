346cd090e86fc6525e7e67233e83d3e88b1f5527
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-13 10:59:34 +0530

    Implemented initial home page view

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_vehicle_makes/controllers/home_page_controller.dart';
+import 'package:flutter_vehicle_makes/models/vehicle_manufacturer.dart';
+import 'package:get/get.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends GetView<HomePageController> {
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -10,9 +13,34 @@ class HomePage extends StatelessWidget {
         leading: const Icon(Icons.home),
         title: const Text('Vehicle Manufacturers'),
       ),
-      body: const Center(
-        child: Text('Home Page'),
+      body: Obx(
+        () => controller.isLoading.value
+            ? Center(
+                child: Text('Loading...'),
+              )
+            : controller.manufacturersList.isEmpty
+                ? Center(child: Text('Oops! Could not display the list'))
+                : Center(
+                    child: ListView.builder(
+                      itemCount: controller.manufacturersList.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        VehicleManufacturer vehicleManufacturer =
+                            controller.manufacturersList[index];
+                        return Card(
+                          child: Column(
+                            children: [
+                              Text(vehicleManufacturer.manufacturerCommonName ??
+                                  vehicleManufacturer.manufacturerName),
+                              Text('${vehicleManufacturer.manufacturerID}')
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                  ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: controller.loadMorePages, label: Text('Load More')),
     );
   }
 }
