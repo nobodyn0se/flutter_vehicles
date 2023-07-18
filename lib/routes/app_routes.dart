d3a970267d6f30094ce92e21038858e8cc32a345
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 01:40:39 +0530

    Refactored details route to use ID for routing

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -6,5 +6,5 @@ abstract class Routes {
   static const SPLASH = '/splash';
 
   static const HOME = '/home';
-  static const DETAILS = '/details/:manufacturer';
+  static const DETAILS = '/details/:id';
 }
