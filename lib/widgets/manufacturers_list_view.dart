b92b9eecf0e82b5d766a0670293cd7e9c0677323
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-18 22:34:32 +0530

    Refactored list view for loaders

--- lib/widgets/manufacturers_list_view.dart
+++ lib/widgets/manufacturers_list_view.dart
@@ -1,38 +1,39 @@
 import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
 
 import '../controllers/home_page_controller.dart';
 import '../models/vehicle_manufacturer.dart';
 import 'manufacturer_card.dart';
 
-class ManufacturersListView extends StatelessWidget {
+class ManufacturersListView extends GetView<HomePageController> {
   const ManufacturersListView({
     super.key,
-    required this.controller,
   });
-
-  final HomePageController controller;
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: ListView.builder(
-            itemCount: controller.viewManufacturersList.length,
-            itemBuilder: (BuildContext context, int index) {
-              VehicleManufacturer vehicleManufacturer =
-                  controller.viewManufacturersList[index];
-              return ManufacturerCard(vehicleManufacturer: vehicleManufacturer);
-            },
+    return Obx(
+      () => Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: controller.viewManufacturersList.length,
+              itemBuilder: (BuildContext context, int index) {
+                VehicleManufacturer vehicleManufacturer =
+                    controller.viewManufacturersList[index];
+                return ManufacturerCard(
+                    vehicleManufacturer: vehicleManufacturer);
+              },
+            ),
           ),
-        ),
-        if (controller.isPageLoading.value)
-          const Center(
-            child: CupertinoActivityIndicator(),
-          ),
-        if (!controller.listHasNextPage.value)
-          const Text('You have reached the end of the list')
-      ],
+          if (controller.isPageLoading.value)
+            const Center(
+              child: CupertinoActivityIndicator(),
+            ),
+          if (!controller.listHasNextPage.value)
+            const Text('You have reached the end of the list')
+        ],
+      ),
     );
   }
 }
