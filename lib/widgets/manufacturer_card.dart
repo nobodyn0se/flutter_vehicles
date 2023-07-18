f7b1e3fa31398a0e8eeb14696164e60f025bc6d6
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 01:42:00 +0530

    Enabled nav from listView to details page with effects

--- lib/widgets/manufacturer_card.dart
+++ lib/widgets/manufacturer_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_vehicle_makes/models/vehicle_manufacturer.dart';
+import 'package:get/get.dart';
 
 class ManufacturerCard extends StatelessWidget {
   const ManufacturerCard({
@@ -18,43 +19,54 @@ class ManufacturerCard extends StatelessWidget {
       ),
       clipBehavior: Clip.antiAliasWithSaveLayer,
       elevation: 7,
-      child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors: [
-                Color(0xFF8672F3),
-                Color(0xFF7763DC),
-                Colors.purpleAccent,
-                Colors.amber,
-              ]),
-        ),
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                vehicleManufacturer.manufacturerCommonName ??
-                    vehicleManufacturer.manufacturerName,
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  //fontFamily: "monospace",
-                ),
+      child: Material(
+        // needed for InkWell ripple effects
+        child: InkWell(
+          onTap: () {
+            // trim fullstops at the end of the arg manufacturer name
+            Get.toNamed('/details/${vehicleManufacturer.manufacturerID}',
+                arguments: vehicleManufacturer.manufacturerName);
+          },
+          child: Ink(
+            // replaces Container to display ripples
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFF8672F3),
+                    Color(0xFF7763DC),
+                    Colors.purpleAccent,
+                    Colors.amber,
+                  ]),
+            ),
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    vehicleManufacturer.manufacturerCommonName ??
+                        vehicleManufacturer.manufacturerName,
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 22,
+                      //fontFamily: "monospace",
+                    ),
+                  ),
+                  Text(
+                    vehicleManufacturer.country,
+                    style: const TextStyle(
+                      color: Color(0xFFDCCECE),
+                      fontSize: 18,
+                    ),
+                  ),
+                  Text('${vehicleManufacturer.manufacturerID}'),
+                ],
               ),
-              Text(
-                vehicleManufacturer.country,
-                style: const TextStyle(
-                  color: Color(0xFFDCCECE),
-                  fontSize: 18,
-                ),
-              ),
-              Text('${vehicleManufacturer.manufacturerID}'),
-            ],
+            ),
           ),
         ),
       ),
