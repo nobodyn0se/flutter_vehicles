6ae1b48777075474b3c6624d25be1ab3d52b00a2
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 05:16:21 +0530

    Configured models page controller for network fetch

--- lib/controllers/models_page_controller.dart
+++ lib/controllers/models_page_controller.dart
@@ -1,11 +1,28 @@
+import 'package:flutter_vehicle_makes/network/services/api_service.dart';
 import 'package:get/get.dart';
 
 class ModelsPageController extends GetxController {
   late String makeName;
 
+  RxBool isLoading = false.obs;
+
+  final ApiService _apiService = Get.find<ApiService>();
+
   @override
   void onInit() {
     super.onInit();
     makeName = Get.arguments;
+  }
+
+  @override
+  void onReady() async {
+    super.onReady();
+    isLoading.value = true;
+    await fetchModelsForMake(makeName);
+    isLoading.value = false;
+  }
+
+  fetchModelsForMake(String makeName) async {
+    final response = await _apiService.getModelsForMake(makeName: makeName);
   }
 }
