6cd66ae46f25196e72bcb4931c2a5dacd4edc811
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-18 23:36:43 +0530

    Refactored vehicle make model

--- lib/models/vehicle_make.dart
+++ lib/models/vehicle_make.dart
@@ -1,9 +1,10 @@
+import 'package:flutter_vehicle_makes/models/response/base_response.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'vehicle_make.g.dart';
 
 @JsonSerializable()
-class VehicleMake {
+class VehicleMake extends BaseResponse {
   @JsonKey(name: 'Make_ID')
   int makeID;
 
@@ -16,7 +17,11 @@ class VehicleMake {
   VehicleMake(
       {required this.makeID,
       required this.makeName,
-      required this.manufacturerName});
+      required this.manufacturerName,
+      required int count,
+      required String message,
+      String? searchCriteria})
+      : super(count: count, message: message, searchCriteria: searchCriteria);
 
   factory VehicleMake.fromJson(Map<String, dynamic> json) =>
       _$VehicleMakeFromJson(json);
--- lib/models/vehicle_make.g.dart
+++ lib/models/vehicle_make.g.dart
@@ -10,10 +10,16 @@ VehicleMake _$VehicleMakeFromJson(Map<String, dynamic> json) => VehicleMake(
       makeID: json['Make_ID'] as int,
       makeName: json['Make_Name'] as String,
       manufacturerName: json['Mfr_Name'] as String,
+      count: json['Count'] as int,
+      message: json['Message'] as String,
+      searchCriteria: json['SearchCriteria'] as String?,
     );
 
 Map<String, dynamic> _$VehicleMakeToJson(VehicleMake instance) =>
     <String, dynamic>{
+      'Count': instance.count,
+      'Message': instance.message,
+      'SearchCriteria': instance.searchCriteria,
       'Make_ID': instance.makeID,
       'Make_Name': instance.makeName,
       'Mfr_Name': instance.manufacturerName,
