b0d62bf45655983f1d2fd972563485fed23f53e9
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 04:29:05 +0530

    Listed models page controller with binding

--- lib/bindings/details_page_binding.dart
+++ lib/bindings/details_page_binding.dart
@@ -1,11 +1,13 @@
 import 'package:get/get.dart';
 
 import '../controllers/details_page_controller.dart';
+import '../controllers/models_page_controller.dart';
 
 class DetailsPageBinding extends Bindings {
   @override
   void dependencies() {
     // lazy loading for details page
     Get.lazyPut(() => DetailsPageController());
+    Get.lazyPut(() => ModelsPageController());
   }
 }
