b85156aea48dbeabbd8c09b6848215c9c5a860c1
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 02:24:44 +0530

    Defined a fetch call to get makes for manufacturers on click

--- lib/controllers/details_page_controller.dart
+++ lib/controllers/details_page_controller.dart
@@ -1,7 +1,16 @@
+import 'dart:developer';
+
 import 'package:get/get.dart';
+
+import '../helpers/widget_helper.dart';
+import '../network/services/api_service.dart';
 
 class DetailsPageController extends GetxController {
   late String manufacturerName;
+
+  RxBool isLoading = false.obs;
+
+  final ApiService _apiService = Get.find<ApiService>();
 
   @override
   void onInit() {
@@ -9,5 +18,19 @@ class DetailsPageController extends GetxController {
 
     // we passed the name through route arguments
     manufacturerName = Get.arguments;
+  }
+
+  @override
+  void onReady() async {
+    isLoading.value = true;
+    await fetchMakeForManufacturer(manufacturerName);
+    isLoading.value = false;
+  }
+
+  fetchMakeForManufacturer(String manufacturerName) async {
+    final response = await _apiService.getMakeForManufacturer(
+        manufacturerName: WidgetHelper.parseManufacturerName(manufacturerName));
+
+    log('Make for manufacturers list: ${response}');
   }
 }
