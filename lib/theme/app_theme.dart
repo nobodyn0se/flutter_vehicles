f37ffa7b2dfd9c2a0656ee4012344d4eb68d6175
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 15:18:43 +0530

    Introduced AppTheme class to contain text and app-wide themes

---
+++ lib/theme/app_theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppTextTheme {
+  // placeholder for text themes in the app
+
+  // title text style on light bg
+  static const TextStyle textTitleLight =
+      TextStyle(fontSize: 18, fontWeight: FontWeight.w700);
+
+  static const TextStyle textSubtitleLight = TextStyle(
+      fontSize: 15, fontWeight: FontWeight.w600, color: Colors.blueGrey);
+}
+
+class AppTheme {
+  // placeholder for UI themes in the app
+}
