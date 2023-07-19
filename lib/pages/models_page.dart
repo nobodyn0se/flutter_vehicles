4f7ce3b4d5979c5d1e755ff83eae6dda1dcc70db
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-19 20:02:00 +0530

    Integrated paginated models list view with controller and API

--- lib/pages/models_page.dart
+++ lib/pages/models_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/models_page_controller.dart';
+import '../models/vehicle_model.dart';
 
 class ModelsPage extends GetView<ModelsPageController> {
   const ModelsPage({Key? key}) : super(key: key);
@@ -13,9 +14,31 @@ class ModelsPage extends GetView<ModelsPageController> {
       appBar: AppBar(
         title: Text(controller.makeName),
       ),
-      body: controller.isLoading.value
-          ? Center(child: CupertinoActivityIndicator())
-          : Center(child: Text('Fetched models for this make')),
+      body: Obx(
+        () => controller.isLoading.value
+            ? Center(child: CupertinoActivityIndicator())
+            : ListView.builder(
+                itemCount: controller.viewVehicleModels.length,
+                itemBuilder: (BuildContext context, int index) {
+                  VehicleModel vehicleModel =
+                      controller.viewVehicleModels[index];
+
+                  return ListTile(
+                    title: Text(vehicleModel.modelName),
+                    subtitle: Text(vehicleModel.makeName),
+                  );
+                }),
+      ),
+      floatingActionButton: Obx(
+        () => FloatingActionButton.extended(
+            onPressed: () => !controller.isEndOfList.value
+                ? controller.loadMoreModels()
+                : null,
+            backgroundColor: controller.isEndOfList.value
+                ? Colors.grey
+                : Theme.of(context).primaryColor,
+            label: Text('Load more')),
+      ),
     );
   }
 }
