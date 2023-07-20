27f21c6b16604ba5eec8c58341da1e91a7566aed
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-21 01:58:56 +0530

    Enabled offline support for vehicle models page

--- lib/controllers/models_page_controller.dart
+++ lib/controllers/models_page_controller.dart
@@ -1,8 +1,12 @@
-import 'dart:math';
+import 'dart:developer';
+import 'dart:math' hide log;
 
 import 'package:flutter_vehicle_makes/network/services/api_service.dart';
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
+import '../constants/app_constants.dart';
 import '../models/vehicle_model.dart';
 
 class ModelsPageController extends GetxController {
@@ -18,19 +22,28 @@ class ModelsPageController extends GetxController {
 
   RxBool isEndOfList = false.obs;
 
+  late Box modelsBox;
+
   final ApiService _apiService = Get.find<ApiService>();
 
   @override
   void onInit() {
     super.onInit();
     makeName = Get.arguments;
+
+    modelsBox = Hive.box(AppConstants.MODELS_BOX_NAME);
   }
 
   @override
   void onReady() async {
     super.onReady();
     isLoading.value = true;
-    await fetchModelsForMake(makeName);
+    if (await InternetConnectionChecker().hasConnection) {
+      await fetchModelsForMake(makeName);
+    } else {
+      offlineFetchModelsForMake(makeName);
+      log('Offline fetch');
+    }
     isLoading.value = false;
   }
 
@@ -39,6 +52,13 @@ class ModelsPageController extends GetxController {
 
     if (response != null) {
       vehicleModels = response.vehicleModels;
+
+      // store fetched vehicle models to Hive box
+      final offlineList = modelsBox.get(makeName);
+
+      if (offlineList == null) {
+        modelsBox.put(makeName, vehicleModels);
+      }
 
       viewVehicleModels.addAll(vehicleModels.getRange(currentItems,
           min(vehicleModels.length, currentItems + itemsPerPage)));
@@ -62,6 +82,19 @@ class ModelsPageController extends GetxController {
       viewVehicleModels.addAll(
           vehicleModels.getRange(currentItems, currentItems + itemsPerPage));
 
+      currentItems += itemsPerPage;
+    }
+  }
+
+  offlineFetchModelsForMake(String makeName) {
+    final offlineList = modelsBox.get(makeName);
+
+    if (offlineList != null) {
+      vehicleModels = List<VehicleModel>.from(offlineList);
+      viewVehicleModels.addAll(vehicleModels.getRange(currentItems,
+          min(vehicleModels.length, currentItems + itemsPerPage)));
+
+      isEndOfList.value = (currentItems + itemsPerPage) > vehicleModels.length;
       currentItems += itemsPerPage;
     }
   }
