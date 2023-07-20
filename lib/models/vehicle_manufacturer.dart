1360c2cbda9acd2b16b84581ce1e4452d2fc3bf7
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-20 17:15:19 +0530

    Refactored manufacturer model class for Hive storage

--- lib/models/vehicle_manufacturer.dart
+++ lib/models/vehicle_manufacturer.dart
@@ -1,18 +1,24 @@
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'vehicle_manufacturer.g.dart';
 
+@HiveType(typeId: 0)
 @JsonSerializable()
 class VehicleManufacturer {
+  @HiveField(0)
   @JsonKey(name: 'Country')
   String country;
 
+  @HiveField(1)
   @JsonKey(name: 'Mfr_CommonName')
   String? manufacturerCommonName;
 
+  @HiveField(2)
   @JsonKey(name: 'Mfr_ID')
   int manufacturerID;
 
+  @HiveField(3)
   @JsonKey(name: 'Mfr_Name')
   String manufacturerName;
 
--- lib/models/vehicle_manufacturer.g.dart
+++ lib/models/vehicle_manufacturer.g.dart
@@ -3,6 +3,53 @@
 part of 'vehicle_manufacturer.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class VehicleManufacturerAdapter extends TypeAdapter<VehicleManufacturer> {
+  @override
+  final int typeId = 0;
+
+  @override
+  VehicleManufacturer read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return VehicleManufacturer(
+      country: fields[0] as String,
+      manufacturerCommonName: fields[1] as String?,
+      manufacturerID: fields[2] as int,
+      manufacturerName: fields[3] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, VehicleManufacturer obj) {
+    writer
+      ..writeByte(4)
+      ..writeByte(0)
+      ..write(obj.country)
+      ..writeByte(1)
+      ..write(obj.manufacturerCommonName)
+      ..writeByte(2)
+      ..write(obj.manufacturerID)
+      ..writeByte(3)
+      ..write(obj.manufacturerName);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VehicleManufacturerAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
