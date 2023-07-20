0b2779cdbfe4768b1360c77b8cc40404717d6ebf
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-20 23:56:31 +0530

    Configured vehicle make class for Hive storage

--- lib/models/vehicle_make.dart
+++ lib/models/vehicle_make.dart
@@ -1,15 +1,20 @@
+import 'package:hive/hive.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'vehicle_make.g.dart';
 
+@HiveType(typeId: 2)
 @JsonSerializable()
 class VehicleMake {
+  @HiveField(0)
   @JsonKey(name: 'Make_ID')
   int makeID;
 
+  @HiveField(1)
   @JsonKey(name: 'Make_Name')
   String makeName;
 
+  @HiveField(2)
   @JsonKey(name: 'Mfr_Name')
   String manufacturerName;
 
--- lib/models/vehicle_make.g.dart
+++ lib/models/vehicle_make.g.dart
@@ -3,6 +3,50 @@
 part of 'vehicle_make.dart';
 
 // **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class VehicleMakeAdapter extends TypeAdapter<VehicleMake> {
+  @override
+  final int typeId = 2;
+
+  @override
+  VehicleMake read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return VehicleMake(
+      makeID: fields[0] as int,
+      makeName: fields[1] as String,
+      manufacturerName: fields[2] as String,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, VehicleMake obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.makeID)
+      ..writeByte(1)
+      ..write(obj.makeName)
+      ..writeByte(2)
+      ..write(obj.manufacturerName);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VehicleMakeAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
+
+// **************************************************************************
 // JsonSerializableGenerator
 // **************************************************************************
 
