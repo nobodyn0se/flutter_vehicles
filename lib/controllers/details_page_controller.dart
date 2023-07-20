ac3906287963afb9b8e8c3c8da62f3a5090f7507
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-20 23:58:38 +0530

    Enabled offline storage support for vehicle makes (details page)

--- lib/controllers/details_page_controller.dart
+++ lib/controllers/details_page_controller.dart
@@ -1,6 +1,8 @@
 import 'dart:developer';
 
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 import '../helpers/widget_helper.dart';
 import '../models/vehicle_make.dart';
@@ -13,20 +15,30 @@ class DetailsPageController extends GetxController {
   RxBool isLoading = false.obs;
   RxBool listHasMakes = true.obs;
 
+  late Box manufacturersBox;
+
   final ApiService _apiService = Get.find<ApiService>();
 
   @override
-  void onInit() {
+  void onInit() async {
     super.onInit();
 
     // we passed the name through route arguments
     manufacturerName = Get.arguments;
+
+    manufacturersBox = Hive.box('VehicleManufacturers');
   }
 
   @override
   void onReady() async {
     isLoading.value = true;
-    await fetchMakeForManufacturer(manufacturerName);
+    if (await InternetConnectionChecker().hasConnection) {
+      await fetchMakeForManufacturer(manufacturerName);
+      log('Online fetch');
+    } else {
+      offlineFetchMakesForManufacturers(manufacturerName);
+      log('Offline fetch');
+    }
     isLoading.value = false;
   }
 
@@ -40,6 +52,26 @@ class DetailsPageController extends GetxController {
       for (var make in vehicleMakeList) {
         log('${make.makeName}, ${make.manufacturerName}');
       }
+
+      final offlineList = manufacturersBox.get(manufacturerName);
+
+      if (offlineList == null) {
+        // save the list of makes with manufacturer's name as the key
+        manufacturersBox.put(manufacturerName, vehicleMakeList);
+      }
+    } else {
+      listHasMakes.value = false;
+    }
+  }
+
+  offlineFetchMakesForManufacturers(String manufacturerName) {
+    var offlineList = manufacturersBox.get(manufacturerName);
+
+    if (offlineList != null) {
+      vehicleMakeList = List<VehicleMake>.from(offlineList);
+      log('Running offline make fetch: ${vehicleMakeList.length}');
+
+      listHasMakes.value = true;
     } else {
       listHasMakes.value = false;
     }
