5bc85f814629789b199c4342a82e2c093b12fffe
nobodyn0se <nobodyn0se@protonmail.ch>
2023-07-21 01:16:17 +0530

    Defined box names in AppConstants placeholder class

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,1 +1,6 @@
-const String BASE_URL = 'https://vpic.nhtsa.dot.gov/api/vehicles';
+class AppConstants {
+  static const String BASE_URL = 'https://vpic.nhtsa.dot.gov/api/vehicles';
+
+  static const String MANUFACTURERS_BOX_NAME = 'VehicleManufacturersBox';
+  static const String MODELS_BOX_NAME = 'VehicleModelsBox';
+}
